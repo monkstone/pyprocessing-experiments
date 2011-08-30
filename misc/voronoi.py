952f044ca56f76e1059482c958f766dfc11f5855
monkstone <martin_p@lineone.net>
2011-08-30 15:51:41 +0100

    revision to voronoi prior to sort experiment

--- misc/voronoi.py
+++ misc/voronoi.py
@@ -27,43 +27,43 @@ B = 2
 
 def setup():
     size(800, 600)
-    global source,  image_array,  npos,  nc,  n,  voronoi
+    global source,  image_array,  npos,  ncol,  n,  voronoi
     voronoi = None
     source = loadImage(path.join(data_path,  'voronoi.png') ) 
     n = randint(50, 100)                              # randomly determine number of cells
     npos = [[None] * n for x in range(2)]   # initialize empty multi arrays 
     nc= [[None] * n for x in range(3)]        # avoid  copy by reference trap (python bites)
-
+    ncol = [None] * n
     for i in xrange(n):
         npos[X][i] = randint(0, width - 1)
         npos[Y][i] = randint(0, height - 1)
-        nc[R][i] = randint(0, 255)
-        nc[G][i] = randint(0, 255)
-        nc[B][i] = randint(0, 255)
-
+        nc[R][i] = randint(0, 255)                             # random red
+        nc[G][i] = randint(0, 255)                             #random green
+        nc[B][i] = randint(0, 255)                             # random blue
+        ncol[i] = nc[R][i]<<16|nc[G][i]<<8|nc[B][i]  # bit shifting to a RGB color
 
 def draw():
     """
     Loads an existing voronoi image from file to keep the impatient happy,  creates 
     (it takes a while) a new voronoi image by brute force, and draws it screen.
     """    
-    global source,  voronoi, npos,  nc,  n
+    global source,  voronoi, npos,  ncol,  n
     if (frame.count < 5):
         background(source)      
     elif (voronoi == None):
-        voronoi = new_voronoi(npos, nc,  n)   # this is a canditate for a new thread!
+        voronoi = new_voronoi(npos, ncol,  n)   # this is a candidate for a new thread!
     else:
         background(voronoi)  
         
-def new_voronoi(npos, nc,  n):
+def new_voronoi(npos, ncol,  n):
     """
     Creates a voronoi image by brute force
-    @param npos = x, y position array voronoi centers
-    @param nc = r, g, b color array
+    @param npos = x, y position array voronoi cell centers
+    @param ncol = RGB color int
     @param  n = no of voronoi cells
-    @return voronoi Pimage
+    @return new voronoi Pimage
     """
-    voronoi = PImage(width,height,  RGB)
+    voronoi = PImage(width, height,  RGB)
     for y in xrange(height):
         for x in xrange(width):
             dmin = ((width - 1) * (width - 1)) + ((height - 1) * (height - 1)) 
@@ -73,7 +73,7 @@ def new_voronoi(npos, nc,  n):
                 if d < dmin:
                     dmin = d
                     j = i
-            voronoi.pixels[y*width+x] = color(nc[R][j], nc[G][j], nc[B][j]) 
+            voronoi.pixels[y*width+x] = ncol[j] 
     voronoi.updatePixels()
     return voronoi
 
