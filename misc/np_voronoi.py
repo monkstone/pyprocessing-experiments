ee40949b16228f8e2da2968bdc0da4de673e7c00
monkstone <martin_p@lineone.net>
2011-09-04 21:01:07 +0100

    Further refinement of voronoi sketch

--- misc/np_voronoi.py
+++ misc/np_voronoi.py
@@ -20,26 +20,37 @@ def setup():
     """
     pyprocessing setup
     """
-    size(800,  600)
-    n = randint(200, 300) # number of cells
+    size(400,  300)
+    n = randint(40, 60) # number of cells
     nx = np.random.randint(width, size=(n))      # numpy does randint(low, high) but high is excluded
-    ny = np.random.randint(height, size= (n))    # if high = None, like here low defaults to randint(0, low)
-    nh = np.random.randint(256, size=(3,  n))
+    ny = np.random.randint(height, size= (n))   # if high = None, like here low defaults to randint(0, low)
+    nh = np.random.randint(256, size=(4,  n))
+    #ncolor = np.zeros((n),  dtype=np.uint64)                       # on a 64 bit system it pays to specify 32 bits
+    nimage = np.zeros((width * height,  3),  dtype=np.uint)
+#    for i in xrange(n)32
+#        ncolor[i] = 255<<24|nh[0][i]<<16|nh[1][i]<<8|nh[2][i]
 
     for y in xrange(height):
+        min = 0 if (y < 0.5 * height) else int(n * 0.1)           # slight speedup if don't need to check  
+        max = int(n * 0.5) if (y < 0.13 * height) else n        # every single particle center each time
         for x in xrange(width):
             # find the closest cell center
             dmin = hypot_squared(width - 1, height - 1)                       # more intelligible version
             #dmin = ((width - 1) * (width - 1)) + ((height - 1) * (height - 1))  # optimized version?
             j = -1
-            for i in xrange(n):
+            for i in xrange(min,  max):
                 d = hypot_squared((nx[i] - x),  (ny[i] - y))                  # more intelligible version
                 #d = ((nx[i] - x) * (nx[i] - x)) + ((ny[i] - y) * (ny[i] - y))  # optimized version?
                 if d < dmin:
                     dmin = d
                     j = i
-            setScreen(x, y, color(nh[0][j], nh[1][j], nh[2][j])) 
-
+                    nimage[width * y + x][0],  nimage[width * y + x][1],  nimage[width * y + x][2] = nh[0][j] << 16,  nh[1][j] << 8, nh[2][j] 
+                    
+    new = createImage(width,height, 'RGB')
+    new.pixels = np.fromstring(nimage, dtype=ctypes.c_uint)
+    new.updatePixels()
+    image(new,  0,  0)
+    
 def hypot_squared(a, b):
     """
     Must be cheaper to calculate than hypot
--- misc/voronoi.py
+++ misc/voronoi.py
@@ -15,7 +15,8 @@ from os import getcwd,  path
 from random import randint
 # this sketch uses the vanilla processing convention of a local 'data' folder
 # where images etc should be stored, and python path.join to create a 
-# cross platform address, works for me!!!
+# cross platform address, depends working directory being where script is located
+# and that directory containing the data folder
 
 data_path = path.join(getcwd(), 'data')  
 
@@ -24,36 +25,51 @@ Y = 1
 R = 0
 G = 1
 B = 2
+display_center = False
 
 def setup():
+    """
+    pyprocessing setup
+    """
     size(800, 600)
-    global source,  image_array,  npos,  ncol,  n,  voronoi
+    global original,  image_array,  npos,  ncol,  n,  voronoi
     voronoi = None
-    source = loadImage(path.join(data_path,  'voronoi.png') ) 
-    n = randint(50, 100)                              # randomly determine number of cells
-    npos = [[None] * n for x in range(2)]   # initialize empty multi arrays 
-    nc= [[None] * n for x in range(3)]        # avoid  copy by reference trap (python bites)
+    original = loadImage(path.join(data_path, 'voronoi.png') ) 
+    n = randint(50, 100)                   # randomly determine number of cells
+    nc= [[None] * n for i in range(3)]     # initialize empty multi arrays     
     ncol = [None] * n
+    np =[[None] * 2 for j in range(n)]     # avoid  copy by reference trap (python bites)
     for i in xrange(n):
-        npos[X][i] = randint(0, width - 1)
-        npos[Y][i] = randint(0, height - 1)
+        np[i][X], np[i][Y]  = randint(0, width - 1),  randint(0, height - 1)
         nc[R][i] = randint(0, 255)                             # random red
-        nc[G][i] = randint(0, 255)                             #random green
+        nc[G][i] = randint(0, 255)                             # random green
         nc[B][i] = randint(0, 255)                             # random blue
         ncol[i] = nc[R][i]<<16|nc[G][i]<<8|nc[B][i]  # bit shifting to a RGB color
-
+    npos = sorted(np, compare_pixel_index)
+    
 def draw():
     """
-    Loads an existing voronoi image from file to keep the impatient happy,  creates 
-    (it takes a while) a new voronoi image by brute force, and draws it screen.
+    Loads an existing voronoi image from file to keep the impatient happy, creates 
+    (it takes a while) a new voronoi image by brute force, and draws it screen. This
+    is the draw loop so we can change things here using key/mouse etc
     """    
-    global source,  voronoi, npos,  ncol,  n
+    global original, voronoi, npos,  ncol,  n
     if (frame.count < 5):
-        background(source)      
+        background(original)      
     elif (voronoi == None):
         voronoi = new_voronoi(npos, ncol,  n)   # this is a candidate for a new thread!
     else:
-        background(voronoi)  
+        background(voronoi)
+    if (display_center):
+        fill(0)
+        for vc in npos:
+            ellipse(vc[X], vc[Y], 5, 5)
+       
+def  compare_pixel_index(a,  b):
+    """
+    Used to sort voronoi centers by their pixel index 
+    """
+    return cmp(a[1]*a[0] +a[0],  b[1]*b[0] +b[0])
         
 def new_voronoi(npos, ncol,  n):
     """
@@ -62,21 +78,44 @@ def new_voronoi(npos, ncol,  n):
     @param ncol = RGB color int
     @param  n = no of voronoi cells
     @return new voronoi Pimage
+    There is a possible speed up if we don't check every voronoi center
+    see commented out code, the cut off maxima/minima were empirically 
+    determined, if more than one center or no center is seen in a cell adjust 
+    the appropriate cut off below code lines 85 to 88.
     """
     voronoi = PImage(width, height,  RGB)
+    # minima = 0            # to get a slight speed up we don't check every cell center
+    # maxima = int(0.6 * n) # minima and maxima determine which values are checked 
     for y in xrange(height):
+        #if (y > 0.1 * height):  
+        #    maxima = int(n * 0.8) if (y < 0.15 * height) else n       
+        #    if (y > 0.8 * height):
+        #        minima = int(n * 0.1) if (y < 0.9 * height) else int(n * 0.15)  
         for x in xrange(width):
             dmin = ((width - 1) * (width - 1)) + ((height - 1) * (height - 1)) 
-            j = -1
+            j = -1                
             for i in xrange(n):
-                d = ((npos[X][i] - x) * (npos[X][i] - x)) + ((npos[Y][i] - y) * (npos[Y][i] - y)) 
+            #for i in xrange(minima, maxima):  
+                d = ((npos[i][X]- x) * (npos[i][X]- x)) + ((npos[i][Y]- y) * (npos[i][Y]- y)) 
                 if d < dmin:
                     dmin = d
                     j = i
             voronoi.pixels[y*width+x] = ncol[j] 
     voronoi.updatePixels()
     return voronoi
-
+    
+def keyPressed():
+    """
+    Press 'c' to toggle the display of voronoi cell "centers" 
+    Press 's' to save the current file to disk
+    """
+    global display_center
+    if key.char in 'cC':
+        display_center = not display_center
+    if key.char in 'sS':
+        global voronoi
+        voronoi.save(path.join(data_path, "new_voronoi.png"))
+    if key.char in 'eE':
+        exit(0)
 run()
-
 
